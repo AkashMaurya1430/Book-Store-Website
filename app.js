2110abcf0a98da2688412ed10075be1af5b6546e
AkashMaurya1430 <akashmaurya1430@gmail.com>
2020-07-21 18:33:21 +0530

    Mongo Connection

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const path = require("path");
+const mongoose = require('mongoose');
 
 
 app.use(express.static(path.join(__dirname, 'public')));
@@ -13,6 +14,21 @@ app.set("view engine", "ejs");
 app.get('/', (req, res) => {
     res.render('homepage');
 });
+app.get('/readbook', (req, res) => {
+    res.render('book');
+})
+
+const MONGODB_URI = "mongodb+srv://Akash3030:Akash3030@book-store.uxbek.mongodb.net/<dbname>?retryWrites=true&w=majority";
+
+mongoose.connect(MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+mongoose.connection.on('connected', () => {
+    console.log("DB connected");
+});
+
 
 let port = (process.env.PORT || '3000')
 
