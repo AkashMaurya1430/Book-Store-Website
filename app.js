2ffbc8116ca69e3c87266ad08c117e869daacd58
AkashMaurya1430 <akashmaurya1430@gmail.com>
2020-07-21 12:17:10 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+const path = require("path");
+
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'views')));
+app.use(express.static(path.join(__dirname, 'js')));
+
+// view engine setup
+app.set("view engine", "ejs");
+
+app.get('/', (req, res) => {
+    res.render('homepage');
+});
+
+let port = (process.env.PORT || '3000')
+
+app.listen(port, process.env.IP, function () {
+    console.log("server is running");
+});
