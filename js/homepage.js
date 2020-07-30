4803d2c6d97d96894b5b75159457af37a2822a10
AkashMaurya1430 <akashmaurya1430@gmail.com>
2020-07-30 10:03:04 +0530

    Final Commit

--- js/homepage.js
+++ js/homepage.js
@@ -11,7 +11,7 @@ function addChapter() {
   col6.className = "col-6 mt-2";
   col6.innerHTML = `<div class="row no-gutters">
     <label for="chapterTitle">Chapter<span></span> Title</label>   
-    <input type="text" class="form-control" id="chapterTitle" placeholder="Title"
+    <input type="text" class="form-control" id="chapterTitle" placeholder="Name Of Chapter"
       name="chapterName" >
     <div class="invalid-feedback">
       Give Chapter Title
@@ -19,7 +19,7 @@ function addChapter() {
   </div>
   <div class="row no-gutters mt-2">
     <label for="story">Story</label>
-    <textarea name="story" id="story" class="form-control col-12" ></textarea>
+    <textarea name="story" id="story" class="form-control col-12 p-1 pl-2" placeholder="Story" ></textarea>
     <div class="invalid-feedback">
       Type your Story
     </div>
